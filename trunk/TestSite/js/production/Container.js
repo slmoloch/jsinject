f9244b520b8b7bc182249b0790cfaf5e85c0e55e
slmoloch <slmoloch@9152ec30-7f8b-11de-b7a7-631be265583c>
2009-08-02 18:23:35 +0000

    - Reuse scope was introduced.

--- trunk/TestSite/js/production/Container.js
+++ trunk/TestSite/js/production/Container.js
@@ -3,38 +3,57 @@
 JsInject = {};
 
 JsInject.Container = function() { 
-    this.factories = [];
+    this.serviceEntries = [];
 };
 
-JsInject.Container.prototype.Register = function(name, factory) {
+JsInject.Container.prototype.Register = function(name, factory, scope) {
     if(this.Registered(name))
     {
         throw "Factory with name '" + name + "' alredy registered";
     }
     
-    this.factories[name] = factory;
+    this.serviceEntries[name] = {factory: factory, scope: scope, instance: null};
 };
 
-JsInject.Container.prototype.Resolve = function(name, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9) {
-    if(!this.Registered(name))
-    {
-        throw "Factory with name '" + name + "' is not registered";
-    }
-    
-    return this.factories[name](this, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9);
+JsInject.Container.prototype.Resolve = function(name, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9)
+{
+    return this.ResolveInternal(name, true, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9);
 };
 
-JsInject.Container.prototype.TryResolve = function(name) {
+JsInject.Container.prototype.TryResolve = function(name, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9) 
+{
+    return this.ResolveInternal(name, false, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9);
+};
+
+JsInject.Container.prototype.ResolveInternal = function(name, throwIfNotRegistered, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9) {
     if(!this.Registered(name))
     {
-        return null;
+        if(throwIfNotRegistered)
+        {
+            throw "Factory with name '" + name + "' is not registered";
+        }
+        else
+        {
+            return null;
+        }
     }
     
-    return this.factories[name](this);
+    var entry = this.serviceEntries[name];  
+    if(entry.scope === "container")
+    {
+        if(entry.instance === null)
+        {
+            entry.instance = entry.factory(this, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9);
+        }
+        
+        return entry.instance;
+    }
+
+    return entry.factory(this, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9);
 };
 
 JsInject.Container.prototype.Registered = function(name) {
-	return this.factories[name] !== undefined;
+	return this.serviceEntries[name] !== undefined;
 };
 
 // Factory registration
@@ -45,10 +64,9 @@ JsInject.Registration = function(name, factory)
     this.scope = "none";
 };
 
-JsInject.Registration.prototype.ReusedWithinContainer = function() 
+JsInject.Registration.prototype.Reused = function() 
 {
     this.scope = "container";
-    
     return this;
 };
 
@@ -72,7 +90,9 @@ JsInject.ContainerBuilder.prototype.Create = function()
     
     for (var x in this.registrations)
     {
-        container.Register(this.registrations[x].name, this.registrations[x].factory);
+        var registration = this.registrations[x];
+        
+        container.Register(registration.name, registration.factory, registration.scope);
     }
     
     return container;
